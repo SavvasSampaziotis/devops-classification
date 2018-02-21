01627489feee620f2c2fa1245574481bbb505b18
Savvas Sampaziotis <savvas456pentium@gmail.com>
2018-02-21 16:01:57 +0200

    Fixed Scale

--- utilities/load_and_omit_dataset.R
+++ utilities/load_and_omit_dataset.R
@@ -13,14 +13,20 @@ C = c("character", "character", "numeric", "numeric", "integer", "integer",
       "integer", "integer", "integer", "integer", "integer", "integer", "integer")
 dataset_g = read.csv("./data/dataset.csv", sep=';', header = TRUE, na.strings = c("NA"), colClasses = C)
 
-# Extract the names of the 240 repository in the dataset.
-rep_names = unique(dataset_g[,2], incomparables = FALSE)
-
 # Feature No.4 ismore than 70% NaN, so it will be excluded from dataset.
 data_t=dataset_g[,-4]
 
-# Exclude Feat No.2: the usernames of each repo do not add any value in the process.
-# data_t <- data_t[,-2]
-
 # Exlcude Records that have NA values
 data_t=data_t[complete.cases(data_t),]
+
+# Extract the names of the 240 repository in the dataset.
+rep_names = unique(data_t[,2], incomparables = FALSE)
+
+# certain repositories have NaN entries. These 
+#  are Rep's No.30 and 90 and they are 
+#  completely removed from the dataset.
+
+
+
+
+
--- utilities/scale_dataset.R
+++ utilities/scale_dataset.R
@@ -11,52 +11,92 @@
 #
 #
 
-rm(list = ls());
-
-C = c("character", "character", "numeric", "numeric", "integer", "integer",
-      "integer", "numeric", "numeric", "integer", "integer", "integer", "integer",
-      "integer", "integer", "numeric", "integer", "integer", "integer", "integer",
-      "integer", "integer", "integer", "integer", "integer", "integer", "integer")
-
-dataset_g = read.csv("./data/dataset.csv", sep=';', header = TRUE, na.strings = c("NA"), colClasses = C)
-
-# Feature No.4 ismore than 70% NaN, so it will be excluded from dataset.
-data_t=dataset_g[,-4]
-
-# Exlcude Records that have NA values
-data_t=data_t[complete.cases(data_t),]
+source('./utilities/load_and_omit_dataset.R')
+# source('./utilities/softmax_norm.R')
 
 
-# Each repository sub-dataset is scaled individually. 
-rep_names = unique(data_t[,2], incomparables = FALSE)
+softmax_norm <- function(subset, r){
+  
+  temp = subset
+  
+  # Perform SD-Scale and Centering.
+  subset_sc = scale(x=subset, center = TRUE)   # x' = (x-E[x])/sd(x)
+  
+  complete_cases =complete.cases(subset_sc)
+  if(sum(complete_cases)==0)
+  {
+    # Case of zero variance for all features... 
+    return(scale(x=subset, center = TRUE, scale = rep(1,24)))
+  }
+    
+  
+  subset_sc = subset_sc[complete_cases,]
+  
+  # Perform Exp scale
+  subset_sc = scale(x=subset_sc, scale = rep(r,24)) # x' = x/r/sd(x)
+  subset_sc <- 1/(1+exp(-subset_sc))
+  
+  # par(mfcol=c(2,1),mar = rep(2, 4))
+  # boxplot(temp)
+  # boxplot(subset_sc)
+  # 
+  return(subset_sc)
+}
+
+
+X = rep(1,24)
+
 scaled_data=data_t[,3:26]
-for(i in 1:240)
+for(i in 1:length(rep_names))
 {
   # This sub-dataset contains all the records from the i-th repository.
-  subset <- data_t[data_t$repository_name==rep_names[i],]
-  
-  # Outlier Removal
-  # TODO:
+  # subset <- data_t[data_t$repository_name==rep_names[i],]
   
   colNums=which(data_t$repository_name==rep_names[i])
   if(sum(colNums)>0)
   {
     
-    scaled_data[colNums,] <- scale(scaled_data[colNums,], center = FALSE)
+    # scaled_data[colNums,] <- scale(scaled_data[colNums,], center = FALSE)
+    
+    scaled_data[colNums,] <- softmax_norm(scaled_data[colNums,], r = 2)
+ 
+    
+    for(f in 3:26)
+    {
+      
+      std_feat = var(x=data_t[colNums,f])
+      
+      # TODO: This should be removed      
+      if(is.na(std_feat))
+      {
+        # X[f] = 0
+        # print(f)
+      }
+      else
+      {
+
+        X[f] = X[f] & (std_feat > 0.1)
+          
+      }
+
+
+    }
     
   }
+  else
+    print(rep_names[i])
+  
+ 
+  
+  # print("savvas")
 }
+
+X = which(abs(scaled_data[,1])>1)
+scaled_data = scaled_data[-X,]
 
 # Scale normalises each data-column by its variance. More NaN values may be generated as result.
 omited_data=colSums(is.na(scaled_data))
-scaled_data=scaled_data[complete.cases(scaled_data),]
 # print(x=c('Number of ommited scaled data:', omited_data))
 
-# Remove all variables from workspace EXCPET scaled_data
-# rm(list = ls()[!(ls()=="scaled_data")])
-
-
-
-
-
+plot(scaled_data)
 
